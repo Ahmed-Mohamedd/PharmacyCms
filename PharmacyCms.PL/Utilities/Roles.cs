e53cfd944cbad87588ac9005ce91b8f7f1d6a859
Aya Saad <ayasa3d5@gmail.com>
2023-11-25 00:24:01 +0200

    Adding Roles Class

---
+++ PharmacyCms.PL/Utilities/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace PharmacyCms.PL.Utilities
+{
+    public static class Roles
+    {
+        public static string Customer { get; private set; } = "Customer";
+        public static string Admin { get; private set; } = "Admin";
+    }
+}
